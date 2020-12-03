0d550c6bab794aa9b0da5e35f5dbe263c29df6ae
Melissa F <melissa.faulner@gmail.com>
2020-12-03 18:26:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,7 +23,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
  data.each_with_object({}) do |(key, value), final_array|
-  binding.pry
+  # binding.pry
   value.each do |inner_key, name_group|
     name_group.each do |pigeon_name|
       binding.pry
